2dc6118b5ffd46e4e200025d9fe3f67429ce0b40
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-27 12:43:06 +0530

    Added multiple states and input change handlers to the ExpenseForm component

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
-                <div className="new-expense__control">
+                <div className="new-expense__control" onChange={titleChangeHandler}>
                     <label>Title</label>
                     <input type="text" />
                 </div>
-                <div className="new-expense__control">
+                <div className="new-expense__control" onChange={amountChangeHandler}>
                     <label>Amount</label>
                     <input type="number" min="0.01" step="0.01" />
                 </div>
-                <div className="new-expense__control">
+                <div className="new-expense__control" onChange={dateChangeHandler}>
                     <label>Date</label>
                     <input type="date" min="2019-01-01" max="2022-12-31" />
                 </div>
