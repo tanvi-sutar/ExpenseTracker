1e2eff379bc8773f9ea0c06e0ecc2f3dc26743f7
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-27 16:56:40 +0530

    Added filter to view expenses based on year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+
+    const yearChangeHandler = (newYear) => {
+        console.log("In expenses " + newYear);
+    }
+    
     return (
         <Card className="expenses">
+            <ExpensesFilter onYearChange={yearChangeHandler} />
             { props.expenses.map((expense, i) => <ExpenseItem expense={expense} />) }
         </Card>
     )
