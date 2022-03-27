b9640b3dc7bc6f3406dca86b50b01460ad291f8d
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-27 17:06:01 +0530

    Added year as a state to the Expenses component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState("2019");
 
     const yearChangeHandler = (newYear) => {
-        console.log("In expenses " + newYear);
+        setFilteredYear(newYear);
+        console.log("In Expenses: "+ newYear + " " + filteredYear);
     }
     
     return (
         <Card className="expenses">
-            <ExpensesFilter onYearChange={yearChangeHandler} />
+            <ExpensesFilter defaultYear={filteredYear} onYearChange={yearChangeHandler} />
             { props.expenses.map((expense, i) => <ExpenseItem expense={expense} />) }
         </Card>
     )
