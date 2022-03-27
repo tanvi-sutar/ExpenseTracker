ed19f28bcf82be00ff86c43e3e302cb1adc1242c
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-27 12:55:57 +0530

    Added two-way binding to the input fields in the form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -26,23 +26,26 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         }
-        console.log(expenseData)
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
-                <div className="new-expense__control" onChange={titleChangeHandler}>
+                <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input type="text" value={enteredTitle}  onChange={titleChangeHandler}/>
                 </div>
-                <div className="new-expense__control" onChange={amountChangeHandler}>
+                <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
-                <div className="new-expense__control" onChange={dateChangeHandler}>
+                <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                    <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
