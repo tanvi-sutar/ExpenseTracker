34e672e2f38b950a5df6639f2e571232c8451893
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-28 23:13:46 +0530

    Added functionality to dynamically update expense list when new expense is added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+let DUMMY_EXPENSES = [
+  {"date": new Date(2022, 0, 12), "title" : "Car Insurance", "amount" : 233.3},
+  {"date": new Date(2022, 0, 15), "title" : "Car Insurance", "amount" : 233.3},
+  {"date": new Date(2022, 1, 24), "title" : "Car Insurance", "amount" : 233.3},
+  {"date": new Date(2022, 2, 8), "title" : "Car Insurance", "amount" : 233.3},
+]
+
 function App() {
 
-  let expenses = [
-    {"date": new Date(2022, 0, 12), "title" : "Car Insurance", "amount" : 233.3},
-    {"date": new Date(2022, 0, 15), "title" : "Car Insurance", "amount" : 233.3},
-    {"date": new Date(2022, 1, 24), "title" : "Car Insurance", "amount" : 233.3},
-    {"date": new Date(2022, 2, 8), "title" : "Car Insurance", "amount" : 233.3},
-  ]
+  const [expenses, setExpenses] = useState([]);
 
   const onAddExpenseHandler = (newExpense) => {
-    expenses.concat(newExpense)
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses]
+    });
     console.log("In app.js\n"+newExpense);
   }
 
