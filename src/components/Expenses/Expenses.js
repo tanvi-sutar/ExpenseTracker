3cfd004a050741333ffa7cbcd3a045bdcad7bbe5
Tanvi Sutar <tanvi@Tanvis-MacBook-Pro.local>
2022-03-29 22:07:25 +0530

    Added conditional content to make the JSX leaner.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,16 +7,26 @@ import "./Expenses.css";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2019");
+    const filteredExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() == filteredYear);
 
     const yearChangeHandler = (newYear) => {
         setFilteredYear(newYear);
         console.log("In Expenses: "+ newYear + " " + filteredYear);
     }
+
+    let expenseContent = <p>No expense found.</p>;
+    if(filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map( filteredExpense => 
+            <ExpenseItem key={filteredExpense.id} expense={filteredExpense} />
+        );
+    }
     
     return (
         <Card className="expenses">
-            <ExpensesFilter defaultYear={filteredYear} onYearChange={yearChangeHandler} />
-            { props.expenses.map((expense, i) => <ExpenseItem expense={expense} />) }
+            <ExpensesFilter 
+                defaultYear={filteredYear} 
+                onYearChange={yearChangeHandler} />
+            {expenseContent}
         </Card>
     )
 }
